20e5b8dc47002e5f21347900c85dc73078d9b511
talha08 <talhaqc@gmail.com>
2018-01-15 04:03:44 +0600

    paper details

--- app/Http/Controllers/LabFrontController.php
+++ app/Http/Controllers/LabFrontController.php
@@ -152,9 +152,11 @@ class LabFrontController extends Controller
     /*==================================================*/
 
     public function supervisor(){
-        $user = User::where('is_teacher', 1)
+         $user = User::where('is_teacher', 1)
                 ->where('status',1)
-                ->simplePaginate(5);
+                ->where('email', '!=','admin@gmail.com')
+                ->orderBy('rank', 'asc')
+                ->simplepaginate(5);
         $news = News::take(3)->orderBy('id','desc')->get();
         return view('labfront.supervisor',compact('user','news'))->with('title','Lab Faculty');
     }
